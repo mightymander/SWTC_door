74dc258f9b7c518e98fcac6ffa3861bafbc458dc
mightymander <jacobdcorbett86@gmail.com>
2024-08-28 22:34:31 +0100

    added some buttons to main page and rough design

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Initial Design Concepts
 - Node.js
 - tailwind
 - mongodb
+
+Run in development mode
+2 termianls
+
+- Nodemon .\app.js
+- npm run build:css
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./views/**/*.ejs"],
+  darkMode: "class", // or 'media' depending on your preference
   theme: {
     extend: {},
   },
